168aee48c2f7167c7bc1174dab1e3cace3062330
Gabriel Kanakares <gabe.kanakares@gmail.com>
2025-07-11 16:05:32 -0500

    now correctly dealing with async token verification

--- Auth/index.mjs
+++ Auth/index.mjs
@@ -6,50 +6,78 @@ import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb';
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
-export const handler = async (event, context, callback) => {
+export const handler = async (event) => {
   const token = event.authorizationToken?.replace("Bearer ", "");
   // no token provided
-  if (!token) return callback("Unauthorized");
+  if (!token) 
+  {
+    console.log("No token found");
+    return {
+      statusCode: 403, 
+      body: JSON.stringify({message: "No token found"})
+    };
+  }
+  console.log("token found");
 
-  jwt.verify(token, getKey, function(err, decoded) {
-    if(err) {
-      console.log(err);
-      callback(err.name);
-    }
-    else {
-      console.log(decoded);
-      // get username so we can look up user id in our own table
-      const params = {
-        TableName: "users",
-        KeyConditionExpression: "cognito_username = :pk",
-        ExpressionAttributeValues: {
-          ":pk": decoded.username
-        },
-        ProjectionExpression: "id"
-      };
+  var decoded;
+  try {
+    decoded = await verifyToken(token, getKey)
+    console.log("token verified");
+  } 
+  catch (err) {
+    console.log(err);
+    return {
+        statusCode: 500,
+        body: JSON.stringify({message : err.name})
+    };
+  }
+  // get username so we can look up user id in our own table
+  const params = {
+    TableName: "users",
+    KeyConditionExpression: "cognito_username = :pk",
+    ExpressionAttributeValues: {
+      ":pk": decoded.username
+    },
+    ProjectionExpression: "id"
+  };
 
-      try{
-        docClient.send(new QueryCommand(params)).then( (data) => {
-          console.log(data)
+  try{
+    var data = await docClient.send(new QueryCommand(params));
+    //.then( (data) => {
+    console.log(data)
 
-          const pathId = event['pathParameters']['id'];
-          const ddbId = data.Items[0].id ?? -1;
-          console.log("Path id = ", pathId);
-          console.log("DDB id = ", ddbId);
-          console.log(pathId);
-          if (pathId == ddbId)
-              callback(null, generatePolicy(decoded.username, 'Allow',  event.methodArn) );
-          else 
-              callback(null, generatePolicy(decoded.username, 'Deny',  event.methodArn) );
-
-        });
+    const pathId = event['pathParameters']['id'];
+    const ddbId = data.Items[0].id ?? -1;
+    console.log("Path id = ", pathId);
+    console.log("DDB id = ", ddbId);
+    console.log(pathId);
+    if (pathId == ddbId){
+      return {
+        statusCode: 200,
+        body: JSON.stringify( generatePolicy(decoded.username, 'Allow', event.methodArn) )
       }
-      catch (err) {
-        console.log(err);
-      }
     }
-  });
+    return {
+      statusCode: 403,
+      body: JSON.stringify( generatePolicy(decoded.username, 'Deny', event.methodArn) )
+    }
+  }
+  catch (err) {
+    return { statusCode: 500, body: JSON.stringify( { message: "Failed to get user id from database"}) };
+  }
 };
+
+// handle asyncronous token stuff
+function verifyToken(token, key) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, key, (err, decoded) => {
+      if (err){
+        return reject(err);
+      }
+      resolve(decoded);
+    });
+  });
+}
 
 // Callback to get public key for token verification
 function getKey(header, callback){
